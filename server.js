2c8fc29e0df547ed8a355f89759eacbdfc4e6e0a
papaponmx <jaimemadrigalrios@gmail.com>
2019-02-04 10:21:07 -0600

    Updated port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ routes.forEach(route => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(process.env.PORT || 3000);
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
