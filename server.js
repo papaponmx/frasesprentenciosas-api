94827bd65c1c298c2ba11b4c976f97f74016e01e
papaponmx <jaimemadrigalrios@gmail.com>
2019-03-04 20:28:00 -0600

    Updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fastify = require('fastify')({
   logger: true,
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const routes = require('./routes');
 // Connect to DB
 console.log('DQTP', PORT); // eslint-disable-line
